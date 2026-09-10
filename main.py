6234cdfa160dda54ed6110feec177b83c7a6ee4b
Chosuey10 <josueealchaer@gmail.com>
2026-09-10 14:13:48 -0300

    1.

--- main.py
+++ main.py
@@ -27,16 +27,28 @@
 VALOR_POR_TALLER = 10000
 COSTO_TALLLER_IA = 5000
 COSTO_TALLLER_PROGRAMACION = 3000
-COSTO_TALLLER_DISEÑO = 4000
+COSTO_TALLLER_DISENO = 4000
 COSTO_TALLLER_JUEGOS = 6000
 CUPOS_POR_TALLER = 45
+cantidad_ia = 0 
+cantidad_programacion = 0 
+cantidad_diseno = 0 
+cantidad_juegos = 0 
+cantidad_tt = 0 
+cantidad_tn = 0 
+cantidad_tm = 0
 
 # Entradas
 
 # Total_alumnos = int(input("Ingrese el total de alumnos: "))
-tipo_taller = input("Ingrese el taller: (1.ia 2.Programacion 3.Diseño grafico 4.)")
+tipo_taller = input("Ingrese el taller: (1.ia 2.Programacion 3.Diseño grafico 4.Juegos: )")
 cantidad_gente_taller = int(input("Ingrese la cantidad de gente por taller: "))
+if cantidad_gente_taller >45:
+    print("Limite de cupos excedido.Ingrese una cantidad <= 45")
+    cantidad_gente_taller = int(input("Ingrese la cantidad de gente por taller: ")) 
+
 turno_seleccionado = input("Ingrese el turno: ")
+
 
 
 #Procesos 
