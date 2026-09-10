bf750e261008184a5cf069486d57732503ce4e9c
Chosuey10 <josueealchaer@gmail.com>
2026-09-10 13:52:25 -0300

    Inicio

---
+++ main.py
@@ -0,0 +1,49 @@
+# ● Algoritmos.
+# ● Entradas.
+# ● Procesos.
+# ● Salidas.
+# ● Variables.
+# ● Tipos de datos.
+# ● Operadores aritméticos.
+# ● Fórmulas y cálculos.
+# ● Conversión de datos.
+# ● Reglas de estilo.
+# Cantidad de alumnos inscriptos en cada taller.
+# Cantidad de alumnos inscriptos en cada turno.
+# Cantidad total de inscripciones.
+# Cupo disponible de cada taller.
+# Porcentaje de cupo utilizado.
+# Dinero total recaudado.
+# Dinero recaudado por cada taller.
+# Ganancia de cada taller.
+# Turno más exitoso de cada taller.
+# Talleres más concurridos.
+# Ranking de los talleres según cantidad de alumnos.
+# Ranking de los turnos según cantidad de alumnos o porcentaje de cupo utilizado.
+
+
+
+
+VALOR_POR_TALLER = 10000
+COSTO_TALLLER_IA = 5000
+COSTO_TALLLER_PROGRAMACION = 3000
+COSTO_TALLLER_DISEÑO = 4000
+COSTO_TALLLER_JUEGOS = 6000
+CUPOS_POR_TALLER = 45
+
+# Entradas
+
+# Total_alumnos = int(input("Ingrese el total de alumnos: "))
+tipo_taller = input("Ingrese el taller: (1.ia 2.Programacion 3.Diseño grafico 4.)")
+cantidad_gente_taller = int(input("Ingrese la cantidad de gente por taller: "))
+turno_seleccionado = input("Ingrese el turno: ")
+
+
+#Procesos 
+    # Ganacia_neta_total= (valor_taller - costo_taller) * Total_alumnos
+    # ganacia_cada_taller = (cantidad_gente_taller * valor_taller)
+
+
+
+
+#Salidas
